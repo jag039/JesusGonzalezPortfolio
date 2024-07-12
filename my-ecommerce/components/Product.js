250f61cd4b54e11dc4abe0b2a73225308106b31f
jag039 <jag039@ucsd.edu>
2024-07-11 22:30:42 -0700

    BUG FIXED

--- my-ecommerce/components/Product.js
+++ my-ecommerce/components/Product.js
@@ -16,7 +16,7 @@ export default function Product({_id, name, price, description, picture}){
       </div>
       <p className="text-sm mt-1 leading-4 text-gray-300">{description}</p>
       <div className="flex mt-1">
-        <div className="text-2xl font-bold grow">${price}</div>
+        <div className="text-2xl font-bold grow text-white">${price}</div>
         <button onClick={() => addProduct()} id="glow" className="mb-4 transition duration-300 ease-in-out hover:bg-blue-950 text-white py-1 px-3 rounded-xl">+</button>
       </div>
     </div>
